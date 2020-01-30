4a16633f63ff18249e78fa10a047704334d95aae
AbhishekKr <abhikumar163@gmail.com>
2020-01-31 01:23:50 +0530

    add dataframe plot, plot-seasonal-decompose and info methods
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- mf_dataframe/__init__.py
+++ mf_dataframe/__init__.py
@@ -1,7 +1,12 @@
 # to provide functionality on creating dataframes for MF data by URL/name
 import pandas as pd
 from pandas.io.json import json_normalize
+import statsmodels.api as sm
+
 from . import mfapi, mod
+
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
 
 
 def from_url(mf_url):
@@ -12,3 +17,25 @@ def from_url(mf_url):
     dataframe.set_index('date', inplace=True)
     df_mf = dataframe.astype(float)
     return mod.fill_null(df_mf)
+
+
+def plot(dataframe):
+    dataframe.plot(figsize=(15, 6))
+    plt.show()
+
+
+def plot_seasonal_decompose(dataframe):
+    decomposition = sm.tsa.seasonal_decompose(dataframe, model='additive')
+    fig = decomposition.plot()
+    plt.show()
+
+
+def info(dataframe):
+    print([dataframe.max()])
+    print([dataframe.min()])
+    print(dataframe.dtypes)
+    print(dataframe.info())
+    print(dataframe.isnull().sum())
+    print(dataframe.head())
+    print(dataframe.index)
+    print(dataframe.head(3))
